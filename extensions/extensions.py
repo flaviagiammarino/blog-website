bea0a59c72d8cd0c15f7f8f120f249565ed1f2ff
flaviagiammarino <flavia.giammarino@gmail.com>
2025-08-26 06:33:00 +0200

    update website

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -2,13 +2,13 @@ def google_analytics(app, pagename, templatename, context, doctree):
     metatags = context.get('metatags', '')
     metatags += """
     <!-- Google tag (gtag.js) -->
-    <script async src="https://www.googletagmanager.com/gtag/js?id=G-2L0F07XRQM"></script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LPMJ6R8T4C"></script>
     <script>
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
-
-      gtag('config', 'G-2L0F07XRQM');
+    
+      gtag('config', 'G-LPMJ6R8T4C');
     </script>
     """
     context['metatags'] = metatags
