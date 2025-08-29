dd2e35f0776bbaef83bfe9231bed27aa5972e780
flaviagiammarino <flavia.giammarino@gmail.com>
2025-08-29 14:14:46 +0200

    update website

--- conf.py
+++ conf.py
@@ -37,7 +37,7 @@ extensions = [
 
 templates_path = ['templates']
 
-exclude_patterns = ['_ablog', 'docs', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['docs', 'Thumbs.db', '.DS_Store']
 
 master_doc = "index"
 highlight_language = 'python3'
