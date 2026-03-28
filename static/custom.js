8c23e639f5f610df997d253e7e3dc7f844da31c2
flaviagiammarino <flavia.giammarino@gmail.com>
2026-03-28 08:54:33 +0100

    update website

--- static/custom.js
+++
@@ -1,6 +0,0 @@
-<!-- Privacy-friendly analytics by Plausible -->
-<script async src="https://plausible.io/js/pa-WdiD6Hmq4r_JzL5dmdLwa.js"></script>
-<script>
-  window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};
-  plausible.init()
-</script>
